7bfd653eff638f24b730f8a85d48f64ef48b57a1
Ridyl <joey.dalatri214@gmail.com>
2024-12-20 16:47:45 -0500

    Server does everything that is required. Still needs some tweaking to correctly pass tests. At the moment I believe it isn't able to access the log.csv file to verify all correct information but other than that it works exactly as expected.

--- server/app.js
+++ server/app.js
@@ -1,37 +1,51 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const csvToJson = require('convert-csv-to-json');
+const logPath = './log.csv'
+
+// check if log file exists
+if(!fs.existsSync(logPath)) {
+    // if the file does not exist we create one
+    fs.writeFile(logPath,'Agent,Time,Method,Resource,Version,Status\n','utf8', (err) => {
+        if (err) console.log(err);
+        else {
+            console.log('New log file created.');
+        }
+    });
+}
 
 app.use((req, res, next) => {
-    const log = {
-        agent : req.headers['user-agent'],
-        time : new Date().toString(),
-        method : req.method,
-        resource : req.originalUrl,
-        version : `HTTP/${req.httpVersion}`,
-        status : 200
-    };
+    // stores all necessary values into an object for readablity and easy access
+    const log =
+        {
+            agent : req.headers['user-agent'],
+            time : new Date().toISOString(),
+            method : req.method,
+            resource : req.originalUrl,
+            version : `HTTP/${req.httpVersion}`,
+            status : 200
+        };
 
-    const appendage = Object.values(log).split(',');
-    console.log(appendage);
+    const logLine = `${log.agent},${log.time},${log.method},${log.resource},${log.version},${log.status}\n`;
 
-    fs.appendFile('log.csv', appendage, (err) => {
-        if (err) {
-            res.status(500).send('unable');
-        }
-        console.log('saved');
+    fs.appendFile(logPath, logLine, (err) => {
+        if (err) console.error('Error writing to log file', err);
     })
+
     next();
 });
 
 app.get('/', (req, res) => {
 // write your code to respond "ok" here
     res.status(200).send('ok');
+    console.log('ok');
 });
 
 app.get('/logs', (req, res) => {
 // write your code to return a json object containing the log data here
-    res.send('no');
+    let json = csvToJson.fieldDelimiter(',').getJsonFromCsv(logPath);
+    res.status(200).json(json);
 });
 
 module.exports = app;
