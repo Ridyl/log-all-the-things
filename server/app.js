422a82718bea48ff729ca8e42a7eaaa5e73f3b29
Ridyl <joey.dalatri214@gmail.com>
2024-12-19 19:32:07 -0500

    The start at properly loggind required request parameters. Needs some tweaking and implementation of writing to the log.csv file.

--- server/app.js
+++ server/app.js
@@ -3,18 +3,35 @@ const fs = require('fs');
 const app = express();
 
 app.use((req, res, next) => {
-// write your logging code here
+    const log = {
+        agent : req.headers['user-agent'],
+        time : new Date().toString(),
+        method : req.method,
+        resource : req.originalUrl,
+        version : `HTTP/${req.httpVersion}`,
+        status : 200
+    };
 
+    const appendage = Object.values(log).split(',');
+    console.log(appendage);
+
+    fs.appendFile('log.csv', appendage, (err) => {
+        if (err) {
+            res.status(500).send('unable');
+        }
+        console.log('saved');
+    })
+    next();
 });
 
 app.get('/', (req, res) => {
 // write your code to respond "ok" here
-
+    res.status(200).send('ok');
 });
 
 app.get('/logs', (req, res) => {
 // write your code to return a json object containing the log data here
-
+    res.send('no');
 });
 
 module.exports = app;
